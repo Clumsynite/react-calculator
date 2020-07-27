7a282e0669248b85adacf9338f6c2ebcd203e412
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-07-27 12:23:43 +0530

    vanilla js functions removed
    adding event handles on each button

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-let app = document.getElementById('app')
+const app = document.getElementById('app')
+const temp = document.getElementById('temp');
+const display = document.getElementById('display');
 
-let hist = [], log = [], c = [];
 
 class Calculator extends React.Component {
   constructor(props){
@@ -8,7 +9,7 @@ class Calculator extends React.Component {
   }
   render(){
     return(
-      <div id="app-body">
+      <div id="calc-body">
         <div id="screen">
           <div id="temp">
           </div>
@@ -41,114 +42,3 @@ class Calculator extends React.Component {
 }
 
 ReactDOM.render(<Calculator/>, app);
-
-(() => {
-   const btns = document.querySelectorAll('button')
-   btns.forEach(btn => {
-     btn.addEventListener('click', () => {
-       const value = btn.textContent
-       calculation(value)
-     })
-   })
-})();
-
-
-const calculation = value => {
-       if(value.match(/^[0-9]$/)){
-         writeNumber(value)
-       }else if(value == '.'){
-         addDecimal()
-       }else if(value.match(/[+\-*/]/)){
-         operation(value)
-       }else if(value == '='){
-         calculate()
-       }else if(value == 'Clear'){
-         clearScreen()
-       }
-}
-
-  const temp = document.getElementById('temp');
-  const display = document.getElementById('display');
-
-const writeNumber = num => {
-  if(display.textContent == '0' || display.textContent.match(/[+\-*/]/)){
-    display.textContent = num
-  }else {
-  display.textContent += num;
-  }
-  temp.textContent += num;
-}
-
-const clearScreen = () => {
-  display.textContent = '0'
-  temp.textContent = ''
-  log = [];
-  hist = []
-}
-const add = (a,b) => {return a+b;}
-const subtract = (a,b) => {return a-b;}
-const multiply = (a,b) => {return a*b;}
-const divide = (a,b) => {return a/b;}
-
-const operator = (a,b,c) => {
-    switch(c){
-        case '+':
-            return add(a,b);
-        case '-':
-            return subtract(a,b);
-        case '*':
-            return multiply(a,b);
-        case '/':
-            return divide(a,b);
-        default:
-            return ;
-    }
-}
-
-const operation = symbol => {
-    if((display.textContent==='0'&&temp.textContent==='')||display.textContent.length>10){return ;}
- 
-  log.push(display.textContent, symbol)
- 
-  hist.push(display.textContent, symbol)
-  if(display.textContent==='0'&&!isNaN(Number(temp.textContent))){
-        display.textContent = log[-1];
-        log.push(display.textContent, symbol);
-    }
-
-  display.textContent = symbol
-  temp.textContent = hist.join('')
-}
-
-const calculate = () => {
-  log.push(display.textContent)
-    let total = 0, temp = 0;
-    if(log[2]=='*'&&log.length==5){log=['5','*','-5']}
-  else if(log[log.length-2]=='+'&&log.length>5&&log[log.length-1]=='5'){
-    log=['5','+','5']
-  }
-  for(let i=0; i<log.length; i++){
-    if(i%2!=0){
-      if(i==1){
-        total += operator(Number(log[i-1]), Number(log[i+1]), log[i]);
-        temp = total;
-      }else {
-        total = operator(temp, Number(log[i+1]), log[i]);
-        temp = total;
-      }
-    }
-  }
-  if(isNaN(temp)){temp= 10}
-  display.textContent = temp;
-  hist.push(log)
-  log = []
-}
-
-const addDecimal = () => {
- if(display.textContent.indexOf('.')<1){
-   display.textContent += '.'
-   temp.textContent += '.'
- }else {
-   display.textContent.replace('.', '')
- }
-}
