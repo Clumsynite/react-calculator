c382006f47149ac8576eebf4cda85f37573571d0
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-07-27 12:27:20 +0530

    added value to each button

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const app = document.getElementById('app')
 const temp = document.getElementById('temp');
 const display = document.getElementById('display');
 
-
 class Calculator extends React.Component {
   constructor(props){
     super(props);
+    this.state = {
+
+    }
   }
   render(){
     return(
@@ -18,23 +20,23 @@ class Calculator extends React.Component {
           </div>
         </div>
         <div id="numpad">
-          <button id="clear">Clear</button>
-          <button id="zero">0</button>
-          <button id="one">1</button>
-          <button id="two">2</button>
-          <button id="three">3</button>
-          <button id="four">4</button>
-          <button id="five">5</button>
-          <button id="six">6</button>
-          <button id="seven">7</button>
-          <button id="eight">8</button>
-          <button id="nine">9</button>
-          <button id="decimal">.</button> 
-          <button id="equals">=</button>
-          <button id="add">+</button>
-          <button id="subtract">-</button>
-          <button id="multiply">*</button>
-          <button id="divide">/</button>
+          <button id="clear" value='C'>Clear</button>
+          <button id="zero" value='0'>0</button>
+          <button id="one" value='1'>1</button>
+          <button id="two" value='2'>2</button>
+          <button id="three" value='3'>3</button>
+          <button id="four" value='4'>4</button>
+          <button id="five" value='5'>5</button>
+          <button id="six" value='6'>6</button>
+          <button id="seven" value='7'>7</button>
+          <button id="eight" value='8'>8</button>
+          <button id="nine" value='9'>9</button>
+          <button id="decimal" value='.'>.</button> 
+          <button id="equals" value='='>=</button>
+          <button id="add" value='+'>+</button>
+          <button id="subtract" value='-'>-</button>
+          <button id="multiply" value='X'>X</button>
+          <button id="divide" value='/'>/</button>
         </div>
       </div>
     )
